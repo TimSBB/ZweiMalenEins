38e3fd4a972744c9c7958a34e18106e612c43e0d
TimSBB <tim-rumpf@gmx.de>
2021-06-24 13:43:51 +0200

    Hover Vibration

--- GefuehlteRealitaet_VR/Assets/Scripts/ChangeColor.cs
+++ GefuehlteRealitaet_VR/Assets/Scripts/ChangeColor.cs
@@ -26,7 +26,7 @@ public class ChangeColor : MonoBehaviour
         gross.GetComponent<Renderer>().material = newMaterial;
         mittel.GetComponent<Renderer>().material = newMaterial;
         klein.GetComponent<Renderer>().material = newMaterial;
-            Debug.Log("NewMaterial " + newMaterial);
+            //Debug.Log("NewMaterial " + newMaterial);
         //change line material
         other.SendMessageUpwards("SetLineMaterial", newMaterial, SendMessageOptions.DontRequireReceiver);
         }
