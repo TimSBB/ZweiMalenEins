59a531c68d098a8cfa994fce5edd8c18ade77353
Yuxi Xia <teresaxyx@yahoo.com>
2021-06-24 13:18:01 +0200

    Change Brush tip for one Player ( no network)

--- GefuehlteRealitaet_VR/Assets/Scripts/ChangeColor.cs
+++ GefuehlteRealitaet_VR/Assets/Scripts/ChangeColor.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class ChangeColor : MonoBehaviour
 {
     public Material newMaterial;
+    private GameObject gross;
+    private GameObject mittel;
+    private GameObject klein;
 
     private void Start()
     {
         newMaterial = this.GetComponent<Renderer>().material;
+
+        gross = GameObject.Find("Gross");
+        mittel = GameObject.Find("Mittel");
+        klein = GameObject.Find("klein");
     }
 
     private void OnTriggerEnter(Collider other)
@@ -16,7 +23,10 @@ public class ChangeColor : MonoBehaviour
         if (other.gameObject.name == "PaintCapsule") { 
         //change brush tip
         other.GetComponent<Renderer>().material = newMaterial;
-        Debug.Log("NewMaterial " + newMaterial);
+        gross.GetComponent<Renderer>().material = newMaterial;
+        mittel.GetComponent<Renderer>().material = newMaterial;
+        klein.GetComponent<Renderer>().material = newMaterial;
+            Debug.Log("NewMaterial " + newMaterial);
         //change line material
         other.SendMessageUpwards("SetLineMaterial", newMaterial, SendMessageOptions.DontRequireReceiver);
         }
--- GefuehlteRealitaet_VR/Assets/Scripts/ChangeWidth.cs
+++ GefuehlteRealitaet_VR/Assets/Scripts/ChangeWidth.cs
@@ -1,17 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ChangeWidth : MonoBehaviour
 {
     public float width;
+    private GameObject gross;
+    private GameObject mittel;
+    private GameObject klein;
+
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == "PaintCapsule") { 
-        //change brush tip
-        //change line material
-        other.SendMessageUpwards("SetLineWidth", width, SendMessageOptions.DontRequireReceiver);
+        gross = GameObject.Find("Gross");
+        mittel = GameObject.Find("Mittel");
+        klein = GameObject.Find("klein");
+
+
+        if (other.gameObject.name == "PaintCapsule") {
+            //change brush tip
+            if (this.gameObject.name == "Gross_Auswahl") 
+            {
+                gross.GetComponent<MeshRenderer>().enabled = true;
+                mittel.GetComponent<MeshRenderer>().enabled = false;
+                klein.GetComponent<MeshRenderer>().enabled = false;
+            }
+            if (this.gameObject.name == "Mittel_Auswahl")
+            {
+                gross.GetComponent<MeshRenderer>().enabled = false;
+                mittel.GetComponent<MeshRenderer>().enabled = true;
+                klein.GetComponent<MeshRenderer>().enabled = false;
+            }
+            if (this.gameObject.name == "klein_Auswahl") 
+            {
+                gross.GetComponent<MeshRenderer>().enabled = false;
+                mittel.GetComponent<MeshRenderer>().enabled = false;
+                klein.GetComponent<MeshRenderer>().enabled = true;
+            }
+            //change line material
+            other.SendMessageUpwards("SetLineWidth", width, SendMessageOptions.DontRequireReceiver);
+
+            //small vibration
+
         }
     }
 }
