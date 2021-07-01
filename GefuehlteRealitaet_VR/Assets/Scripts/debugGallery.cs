0345cd64c705bc85f4f75dbe96a221efccbf6172
TimSBB <tim-rumpf@gmx.de>
2021-07-01 22:51:52 +0200

    gallery debug fix?

--- GefuehlteRealitaet_VR/Assets/Scripts/debugGallery.cs
+++ GefuehlteRealitaet_VR/Assets/Scripts/debugGallery.cs
@@ -1,15 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
 public class debugGallery : MonoBehaviour
 {
     public GameObject gallery;
     public GameObject gallerySchrift;
+    private PhotonView PV;
     // Start is called before the first frame update
     void Start()
     {
-        
+        PV = this.GetComponent<PhotonView>();
     }
 
     // Update is called once per frame
@@ -20,19 +22,40 @@ public class debugGallery : MonoBehaviour
 
     private void Gallery()
     {
-        Instantiate(gallery);
-        Instantiate(gallerySchrift);
+        PV.RPC("RPC_TimesUp", RpcTarget.AllBufferedViaServer);
+        //Instantiate(gallery);
+        //Instantiate(gallerySchrift);
 
-        var transform = GameObject.Find("Drawing").transform;
-        if (transform.childCount > 0)
-        {
-            foreach (Transform child in transform)
-            {
-                Destroy(child.gameObject);
-            }
-        }
+        //var transform = GameObject.Find("Drawing").transform;
+        //if (transform.childCount > 0)
+        //{
+        //    foreach (Transform child in transform)
+        //    {
+        //        Destroy(child.gameObject);
+        //    }
+        //}
 
-        var oldAnweisungen = GameObject.Find("AnweisungWortStatusbar");
-        Destroy(oldAnweisungen);
+        //var oldAnweisungen = GameObject.Find("AnweisungWortStatusbar");
+        //Destroy(oldAnweisungen);
+
     }
+
+
+    [PunRPC]
+    void RPC_TimesUp()
+    {
+            var transform = GameObject.Find("Drawing").transform;
+            if (transform.childCount > 0)
+            {
+                foreach (Transform child in transform)
+                {
+                    Destroy(child.gameObject);
+                }
+            }
+            Instantiate(gallery);
+            Instantiate(gallerySchrift);
+
+        Destroy(GameObject.Find("AnweisungWortStatusbar(Clone)"));
+    }
+
 }
