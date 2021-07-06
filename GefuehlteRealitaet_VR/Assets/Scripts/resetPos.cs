7e4bea17fc34f04053e387adc64e75b12a67ae99
TimSBB <tim-rumpf@gmx.de>
2021-07-06 15:21:56 +0200

    Added origin reset on grip pressed

--- GefuehlteRealitaet_VR/Assets/Scripts/resetPos.cs
+++ GefuehlteRealitaet_VR/Assets/Scripts/resetPos.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class resetPos : MonoBehaviour
 {
@@ -9,25 +10,35 @@ public class resetPos : MonoBehaviour
     public bool sceneSet;
     public bool scene2Set;
     public bool killedInstance = false;
+    private bool resetOrigin = false;
     public GameObject CharacterEditor_Scene_player1;
     public GameObject CharacterEditor_Scene_player2;
+    private GameObject rightHand;
+    private GameObject cam;
+
+    private XRController controller;
+    public InputHelpers.Button resetButton;
+
     // Start is called before the first frame update
     void Start()
     {
         this.transform.eulerAngles = new Vector3(0, 180, 0);
-        var pos = GameObject.Find("Main Camera").transform.position;
+        cam = GameObject.Find("Main Camera");
+        var pos = cam.transform.position;
         this.transform.position = new Vector3(-pos.x, this.transform.position.y, -pos.z);
-
+        rightHand = GameObject.Find("RightHand Controller");
+        controller = rightHand.GetComponent<XRController>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        InputHelpers.IsPressed(controller.inputDevice, resetButton, out bool isPressed);
         playerNr = PhotonNetwork.LocalPlayer.ActorNumber;
         if (playerNr == 1 && !sceneSet)
         {
-            this.transform.position = new Vector3(-1.7f*0, this.transform.position.y, 0);
-            //this.transform.position = new Vector3(-1.7f, this.transform.position.y, 0);
+            //this.transform.position = new Vector3(-1.7f*0, this.transform.position.y, 0);
+
             Instantiate(CharacterEditor_Scene_player1);
             sceneSet = true;
             LoadingOverlay overlay = GameObject.Find("LoadingOverlay").gameObject.GetComponent<LoadingOverlay>();
@@ -39,13 +50,22 @@ public class resetPos : MonoBehaviour
            {
                 LoadingOverlay overlay = GameObject.Find("LoadingOverlay").gameObject.GetComponent<LoadingOverlay>();
                 Destroy(GameObject.Find("CharacterEditor_Scene_player1(Clone)"));
-                this.transform.position = new Vector3(+1.7f*0, this.transform.position.y, 0);
-                //this.transform.position = new Vector3(+1.7f, this.transform.position.y, 0);
+                //this.transform.position = new Vector3(+1.7f*0, this.transform.position.y, 0);
+
                 Instantiate(CharacterEditor_Scene_player2);
                 scene2Set = true;
                 killedInstance = true;
                 overlay.FadeOut();
             }
+        }
+        if (isPressed)
+        {
+            if (!resetOrigin) { 
+            this.transform.RotateAround(cam.transform.position, Vector3.up, -rightHand.transform.eulerAngles.y);
+            this.transform.position = new Vector3(rightHand.transform.position.x, this.transform.position.y, rightHand.transform.position.z);
+                resetOrigin = true;
+            }
+
         }
     }
 }
