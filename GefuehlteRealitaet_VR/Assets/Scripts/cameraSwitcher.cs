17017b6dc444f1a08ecf17642db58fd63880a7f5
TimSBB <tim-rumpf@gmx.de>
2021-07-08 13:11:55 +0200

    added camera switch

---
+++ GefuehlteRealitaet_VR/Assets/Scripts/cameraSwitcher.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class cameraSwitcher : MonoBehaviour
+{
+    private XRController controller;
+    public InputHelpers.Button camerswitchButton;
+    private bool toggle;
+    private float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GameObject.Find("RightHand Controller").GetComponent<XRController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 1)
+        {
+            timer += 0.1f;
+        }
+        InputHelpers.IsPressed(controller.inputDevice, camerswitchButton, out bool isPressed);
+        if (isPressed && timer >= 1)
+        {
+            timer = 0;
+        }
+        if (timer == 0)
+        {
+            toggle = !toggle;
+        }
+        if (toggle)
+        {
+            this.GetComponent<Camera>().depth = 1;
+            GameObject.Find("ActiveCameraDebug").GetComponent<Canvas>().enabled = true;
+        }
+        if (!toggle)
+        {
+            this.GetComponent<Camera>().depth = -1;
+            GameObject.Find("ActiveCameraDebug").GetComponent<Canvas>().enabled = false;
+        }
+    }
+}
